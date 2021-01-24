66309594d3eae19a72a4b1c62abed55d1b09a561
Georgina <gadzorgenu@gmail.com>
2021-01-24 17:03:32 +0000

    Network page

--- messaging-app/src/Components/Login.js
+++ messaging-app/src/Components/Login.js
@@ -1,72 +1,77 @@
 import React, {useState} from 'react'
 import {useHistory} from 'react-router-dom'
 import { Box, Button, Input, SimpleGrid, Textarea, Container, Center} from '@chakra-ui/react'
+import {Formik, Form} from 'formik'
 
 const Login = () => {
-    const [name, setName] = useState(''),
-          [email, setEmail ] = useState(''),
-          [ description, setDescription ] =useState('');
+    
+    const initialValues = {
+      name: '',
+      email: '',
+      description:'',
+      id: Math.ceil(Math.random() *10000),
+      role: 'Member',
+      image: 'https://talkjs.com/docs/img/ronald.jpg'
+    }
 
     const history = useHistory()
-  
-    const handleName=(e)=>{
-        setName(e.target.value)
-    }
-    const handleEmail =(e) =>{
-        setEmail(e.target.value)
-    }
-    const handleDesc = (e)=>{
-        setDescription(e.target.value)
-    }
 
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        let users  = JSON.parse(localStorage.getItem('users'))
+    const onSubmit = (values) =>{
+        let user={...values}
+        console.log(user)
+        /* Store user data in browser's local storage */
+        localStorage.setItem('currentTalkjsUser', JSON.stringify(user))
 
-        // let user = users[name]
-        //generating a random number to serve as user id
-        const randomNumb = Math.ceil(Math.random() *10000)
-
-        // const userData ={
-        //     name,email,description,id: randomNumb, role: 'Member', image:'https://talkjs.com/docs/img/ronald.jpg'
-        // }
-
-        //storing user data in browser's local storage
+        // /*Redirect to the my network page */
         history.push('/network')
+       
     }
     return (
       <Box>
-           <form onSubmit={handleSubmit}>
-               <Container maxW={{ md:'50%'}} p={6} bgColor='gray.100' my='5%' >
-                    <SimpleGrid columns={2} spacing={4} mb={4}>
-                        <Input 
-                            onChange={handleName}
-                            placeholder ='Name'
-                            name='name'
-                            _placeholder={{ color: '#283e4a'}}
-                            borderColor='#283e4a'
-                        />
-                        <Input 
-                            onChange={handleEmail}
-                            placeholder ='Email'
-                            name='email'
-                            _placeholder={{ color: '#283e4a'}}
-                            borderColor='#283e4a'
-                        />
-                    </SimpleGrid>
-                    <Textarea 
-                        borderColor='#283e4a'
-                        onChange={handleDesc}
-                        name='description'
-                        _placeholder={{ color: '#283e4a'}}
-                        placeholder='Short Description'
-                        mb={4}
-                    />
-                    <Center as='a' href='/network'>
-                        <Button type='button' bgColor='#283e4a' color='white' borderRadius='none' px={6} _hover={{ textDecor : 'none'}} > Submit</Button>
-                    </Center>
-                </Container>
-           </form>
+          <Formik initialValues={initialValues}
+                onSubmit= {onSubmit}
+            >
+              {({
+                values,
+                handleSubmit,
+                handleChange,
+              }) => (
+                <Form onSubmit={handleSubmit}>
+                    <Container maxW={{ md:'50%'}} p={6} bgColor='gray.100' my='5%' >
+                            <SimpleGrid columns={2} spacing={4} mb={4}>
+                                <Input 
+                                    placeholder ='Name'
+                                    value={values.name}
+                                    onChange={handleChange}
+                                    name='name'
+                                    _placeholder={{ color: '#283e4a'}}
+                                    borderColor='#283e4a'
+                                />
+                                <Input 
+                                    placeholder ='Email'
+                                    name='email'
+                                    value={values.email}
+                                    _placeholder={{ color: '#283e4a'}}
+                                    borderColor='#283e4a'
+                                    onChange={handleChange}
+                                />
+                            </SimpleGrid>
+                            <Textarea 
+                                borderColor='#283e4a'
+                                name='description'
+                                onChange={handleChange}
+                                value={values.description}
+                                _placeholder={{ color: '#283e4a'}}
+                                placeholder='Short Description'
+                                mb={4}
+                            />
+                            <Center>
+                                <Button type='submit' bgColor='#283e4a' color='white' borderRadius='none' px={6} _hover={{ textDecor : 'none'}} > Submit</Button>
+                            </Center>
+                        </Container>
+                </Form>
+              )}
+          </Formik>
       </Box>
     )
 }
--- messaging-app/src/Components/MyNetwork.js
+++ messaging-app/src/Components/MyNetwork.js
@@ -1,10 +1,63 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {dummyUsers} from './Users'
 import { Box, Avatar, Heading, Text, Button, Flex, Container, Spacer, Divider } from '@chakra-ui/react'
+import Talk from 'talkjs'
 
 const MyNetwork = () =>{
+  const [currentUser] = useState('')
+  let currentUser;
+  const currentTalkjsUser = localStorage.getItem('currentTalkjsUser');
+  if (currentTalkjsUser) {
+  currentUser = JSON.parse(currentTalkjsUser)
+  }
+
+  const handleClick = (userId) =>{
+    /* Retrieve the two users that will participate in the conversation */
+    let currentUser;
+    const user = dummyUsers.find(user => user.id === userId)
+
+    /* Session initialization code */
+    Talk.ready
+    .then(() => {
+    /* Create the two users that will participate in the conversation */
+    const me = new Talk.User(currentUser);
+    const other = new Talk.User(user)
+    
+    /* Create a talk session if this does not exist. Remember to replace tthe APP ID with the one on your dashboard */
+    if (!window.talkSession) {
+    window.talkSession = new Talk.Session({
+    appId: process.env.talkId,
+    me: me
+    });
+    } 
+    
+    /* Get a conversation ID or create one */
+    const conversationId = Talk.oneOnOneId(me, other);
+    const conversation = window.talkSession.getOrCreateConversation(conversationId);
+    
+    /* Set participants of the conversations */
+    conversation.setParticipant(me);
+    conversation.setParticipant(other);
+    
+    /* Create and mount chatbox in container */
+    this.chatbox = window.talkSession.createChatbox(conversation);
+    this.chatbox.mount(this.container);
+    }) 
+    .catch(e => console.error(e));
+    }
+  }
     return (
        <Box >
+         {
+           currentUser && 
+           <Box>
+              <Avatar src={currentUser.photoUrl} alt={currentUser.name}/>
+                <Box>
+                  <Heading>{currentUser.name}</Heading>
+                  <Text>{currentUser.description}</Text>
+                </Box>
+            </Box>
+         }
          <Container m='auto' maxW='40%' my='5%' borderWidth={1} borderColor='#283e4a' p={6} borderRadius={3}>
             {
               dummyUsers.map( user => (
@@ -24,6 +77,7 @@ const MyNetwork = () =>{
                         _hover={{ textDecor: 'none'}}
                         size='sm'
                         mt={6}
+                        onClick={(userId)=> handleClick(user.id)}
                       >
                         Message
                       </Button>
